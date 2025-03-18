5d59198e2ebbb8815798e01d2615160a371bd5c6
LieonJ <99437204+LieonJ@users.noreply.github.com>
2025-03-18 18:42:24 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+const amigos = [];
+
+function adicionarAmigo(){
+    const inputNome = document.getElementById("amigo");
+    const nome = inputNome.value.trim();
+
+    if(nome !== ""){
+        amigos.push(nome);
+        console.log(nome);
+        console.log(amigos);
+        inputNome.value = "";
+        
+    }else{
+        alert("Por favor, insira um nome.");
+    }
+}
